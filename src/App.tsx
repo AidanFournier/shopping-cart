23b3143df9acd2d77089a111092da780c9b270fb
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-09-08 23:41:05 +0900

    Set up basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
 export default function App() {
-  return <h1>Hi</h1>
+  return <Container>
+    <Routes>
+      <Route path="/"  element={<Home />} />
+      <Route path="/store"  element={<Store />} />
+      <Route path="/about"  element={<About />} />
+    </Routes>
+  </Container>
 }
