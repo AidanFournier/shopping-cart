f79433205109395a6644c5286515134e2bc007da
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-09-10 01:15:37 +0900

    Basic navbar complete and styled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import ShoppingCart from "../images/shopping-cart.png"
 
 export function Navbar() {
     return (
-        <NavbarBs className="bg-white shadow-sm mb-3">
+        <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
                 <Nav className="me-auto">
                     <Nav.Link to="/" as={NavLink}>
@@ -16,7 +17,27 @@ export function Navbar() {
                         About
                     </Nav.Link>
                 </Nav>
-                <Button></Button>
+                <Button
+                    style={{ width: "3rem", height: "3rem", position: "relative" }}
+                    variant="light" className="rounded-circle"
+                >
+                    <img src= {ShoppingCart} alt="Shopping cart" className="button-cart" width="25" height="25" style={{ position: "absolute", bottom: 10, right: 11 }}/>
+                    <div 
+                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                        style={{ 
+                            color: "white",
+                            fontSize: "12px",
+                            width: "1rem",
+                            height: "1rem",
+                            position: "absolute",
+                            bottom: 0,
+                            right: 0,
+                            transform: "translate(25%, 25%)"
+                        }}
+                    >
+                        3
+                    </div>
+                </Button>
             </Container>
         </NavbarBs>
     )
