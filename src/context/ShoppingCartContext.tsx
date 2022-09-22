a9fd17dd2c06529edfe19b4067eec4a70aca6d29
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-09-22 18:44:02 -0400

    Fixed bug in shopping cart context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+// import { ShoppingCart } from "../components/ShoppingCart"
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -9,14 +10,14 @@ type CartItem = {
     quantity: number
 }
 
-type ShoppingCartContext = [
+type ShoppingCartContext = {
     getItemQuantity: (id: number) => number,
     increaseCartQuantity: (id: number) => void,
     decreaseCartQuantity: (id: number) => void,
     removeFromCart: (id: number) => void
-]
+}
 
-const ShoppingCartContext = createContext({} as ShoppingCartContext);
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 export function useShoppingCart() {
     return useContext(ShoppingCartContext)
@@ -26,7 +27,7 @@ export function ShoppingCartProvider( { children }: ShoppingCartProviderProps) {
     const [cartItems, setCartItems] = useState<CartItem[]>([])
 
     function getItemQuantity(id: number) {
-        return cartItems.find(item => item.id === id?).quantity || 0
+        return cartItems.find(item => item.id === id)?.quantity || 0
     }
 
     function increaseCartQuantity(id: number) {
