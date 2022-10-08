c4b9075048c500e3b4b055ca711f35911d34689c
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-09 00:23:41 +0900

    Added images to cart items

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "react-bootstrap"
 import { useShoppingCart} from "../context/ShoppingCartContext"
 import storeItems from "../data/items.json"
 
@@ -8,6 +9,15 @@ type CartItemProps = {
 
 export function CartItem({ id, quantity }: CartItemProps) {
     const { removeFromCart } = useShoppingCart()
-    const item = storeItems
+    const item = storeItems.find(i => i.id === id)
+    if (item == null) return null
 
+    return (
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+            <img
+                src={item.imgUrl}
+                style={{ width: "125px", height: "75px", objectFit: "cover" }}
+            />
+        </Stack>
+    )
 }
